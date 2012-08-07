e4d4fcbecfdcbbc09d1a182b2a4e41499fa8f77b
marc <marc@My-Fucking-MacBook-Pro.local>
2012-08-06 22:26:52 -0700

    Adding Brain, images and a datamodel to the new project

---
+++ XKCD Reader 2.0/Brains.h
@@ -0,0 +1,15 @@
+//
+//  Brains.h
+//  XKCD Reader 2.0
+//
+//  Created by marc on 8/6/12.
+//  Copyright (c) 2012 marc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "pcre.h"
+#import "libxml/HTMLParser.h"
+
+@interface Brains : NSObject
+
+@end
---
+++ XKCD Reader 2.0/Brains.m
@@ -0,0 +1,13 @@
+//
+//  Brains.m
+//  XKCD Reader 2.0
+//
+//  Created by marc on 8/6/12.
+//  Copyright (c) 2012 marc. All rights reserved.
+//
+
+#import "Brains.h"
+
+@implementation Brains
+
+@end
